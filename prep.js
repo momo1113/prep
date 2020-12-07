0270d7cddd882211c99bd70bf52c1cc5f2a4789e
Momo Sun <mosun1113@gmail.com>
2020-12-07 14:51:05 -0800

    add first file

---
+++ prep.js
@@ -0,0 +1,16 @@
+//1. create a string
+let myString = 'Hello Momo'
+
+//2. create an array 
+let myArray = ['1','2','3','4']
+
+//3. create an object
+
+let myObject = {
+    myName: 'hello',
+    age: '18',
+    address: 'walnut creek'
+}
+
+//4
+
